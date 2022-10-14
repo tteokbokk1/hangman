d0a8e3a57cf9f35f2ecae642b8325fd03247f8df
Henry <nguyen2858@gmail.com>
2022-10-14 13:01:32 -0700

    add the game logic

---
+++ main.py
@@ -0,0 +1,58 @@
+import random
+import hangman_art
+import hangman_words
+
+print(hangman_art.logo)
+chosen_word = random.choice(hangman_words.word_list)
+
+#make a list with blanks from the randomly selected letter
+display = []
+for letter in range(len(chosen_word)):
+    display.append("_")
+
+#check the player's guess against the word and replace blanks in the list with correct guesses
+game_state = True
+
+lives = 6
+#make a list of past guesses so the player doesn't get punshied for repeated guesses
+guesses = []
+
+while game_state == True:
+    guess = input("Guess a letter\n").lower()
+    
+    skip = False
+    
+    if guess in guesses:
+        skip = True
+
+    position = 0
+    for letter in chosen_word:
+        if letter == guess:
+            display[position] = guess
+            position += 1
+        else:
+            position += 1
+
+    if skip == True:
+        print(f"You've already guessed '{guess}'. Try again")
+        skip = False
+        guesses.append(guess)
+    elif guess not in display:
+        lives -= 1
+        guesses.append(guess)
+        if lives == 0:
+            game_state = False
+            print(hangman_art.game_stages[0])
+            print("You lose.")
+        else:
+            print(hangman_art.game_stages[lives])
+            print(f"{' '.join(display)}")
+            print(f"You guessed '{guess}'. That is not part of the chosen word")
+    elif "_" not in display:
+        game_state = False
+        print(f"{' '.join(display)}")
+        print("You've won!")
+    else:
+        print(hangman_art.game_stages[lives])
+        print(f"{' '.join(display)}")
+        guesses.append(guess)
